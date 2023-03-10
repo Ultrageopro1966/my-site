3289073600b6b9a98104f0e831adce080ed47f81
Ultrageopro1966 <Korney060708@gmail.com>
2023-03-10 11:44:10 +0300

    Привет, друзья

---
+++ code.js
@@ -0,0 +1,7 @@
+let Git = document.getElementById('GitHub');
+let Telegram = document.getElementById('Telegram')
+let Mail = document.getElementById('Mail')
+
+Telegram.addEventListener('click', ()=>{window.location.assign("https://t.me/UltraGeoPro"); console.log("Stephen Wolfram Fell Down")})
+Git.addEventListener('click', ()=>{window.location.assign("https://github.com/Ultrageopro1966"); console.log("Stephen Wolfram Fell Down")})
+Mail.addEventListener('click', ()=>{window.location.assign("mailto:me@geopro1966.online"); console.log("Stephen Wolfram Fell Down")})
