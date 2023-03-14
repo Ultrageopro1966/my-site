8320773db689dbb4a8046854829c090cc39a6733
Ultrageopro1966 <Korney060708@gmail.com>
2023-03-14 17:28:13 +0300

    my-syte

--- code.js
+++ code.js
@@ -1,7 +1,9 @@
 let Git = document.getElementById('GitHub');
 let Telegram = document.getElementById('Telegram')
 let Mail = document.getElementById('Mail')
+let Discord = document.getElementById('Discord')
 
 Telegram.addEventListener('click', ()=>{window.location.assign("https://t.me/UltraGeoPro"); console.log("Stephen Wolfram Fell Down")})
 Git.addEventListener('click', ()=>{window.location.assign("https://github.com/Ultrageopro1966"); console.log("Stephen Wolfram Fell Down")})
 Mail.addEventListener('click', ()=>{window.location.assign("mailto:me@geopro1966.online"); console.log("Stephen Wolfram Fell Down")})
+Discord.addEventListener('click', ()=>{window.location.assign("https://discordapp.com/users/696381136816832592/"); console.log("Stephen Wolfram Fell Down")})
